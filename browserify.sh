2944534d9038672c89eed7873661246072813f33
Mark Macdonald <Mark_MacDonald@ca.ibm.com>
2013-07-22 12:20:17 -0400

    update browserify script

--- browserify.sh
+++ browserify.sh
@@ -1,2 +1,17 @@
-#!/bin/sh
-browserify lib/eslint -s eslint 
+#!/bin/bash
+DIR=`dirname $0`
+ESLINT_PLUGIN=${DIR}/../eslint-plugin
+RJS=${DIR}/node_modules/.bin/r.js
+
+pushd $DIR
+echo
+
+echo Browserifying eslint...
+browserify lib/eslint -s eslint > ${ESLINT_PLUGIN}/lib/orion-eslint.js
+
+echo
+
+echo Converting eslint rules to AMD...
+${RJS} -convert lib/rules/ ${ESLINT_PLUGIN}/lib/rules
+
+popd
